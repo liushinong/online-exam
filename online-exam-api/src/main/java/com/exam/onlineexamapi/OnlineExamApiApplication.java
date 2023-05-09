21a312dc6b444aa7988cca5dcfd569a9118c486d
xuekaikai <2780739366@qq.com>
2023-05-09 11:15:28 +0800

    后端项目搭建

---
+++ online-exam-api/src/main/java/com/exam/onlineexamapi/OnlineExamApiApplication.java
@@ -0,0 +1,13 @@
+package com.exam.onlineexamapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineExamApiApplication.class, args);
+    }
+
+}
---
+++ online-exam-api/src/test/java/com/exam/onlineexamapi/OnlineExamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.onlineexamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
